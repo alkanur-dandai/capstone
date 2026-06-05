162408baf55fc7032289ac1d04c843fe26fdb1b3
dandai <alkanurdandai.acad@gmail.com>
2026-06-05 12:25:20 +0800

    Add PWA support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//    reactStrictMode: false,
+// };
+
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+// });
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+// });
+
+// export default nextConfig;
+
+
+import nextPWA from "next-pwa";
 import type { NextConfig } from "next";
 
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
-   reactStrictMode: false,
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
